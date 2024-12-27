9b3621228248ad6303d57fec3859c6a213a03094
vamseekrishna341 <122243483+vamseekrishna341@users.noreply.github.com>
2024-12-27 13:15:50 +0530

    Update deploy_frontend.sh

--- scripts/deploy_frontend.sh
+++ scripts/deploy_frontend.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# Define S3 bucket for frontend deployment
-S3_BUCKET="my-frontend-bucket"
+# Define EC2 connection details
+EC2_USER="ec2-user"              # For Amazon Linux or RHEL-based EC2 instances, use "ubuntu" for Ubuntu-based instances
+EC2_IP="ec2-10-20-3-207.compute-1.amazonaws.com"  # Replace with your EC2 instance's public IP or DNS name
+EC2_KEY_PATH="C:\Users\Agssysadmin\Downloads\vk.pem"  # Path to your EC2 instance's SSH private key (use the .pem file you downloaded when creating the instance)
+EC2_DEST_DIR="/var/www/html"     # Directory on EC2 where the frontend files will be deployed (commonly used with web servers like Apache/Nginx)
 
-# Sync the React build directory to S3
-echo "Deploying frontend to S3"
-aws s3 sync ./frontend/build/ s3://$S3_BUCKET/ --delete
+# Sync the React build directory to EC2 instance using rsync
+echo "Deploying frontend to EC2"
+rsync -avz -e "ssh -i $EC2_KEY_PATH" ./frontend/build/ $EC2_USER@$EC2_IP:$EC2_DEST_DIR
 
-# In case of EC2 deployment, you could use scp or rsync
-# Example: rsync -avz ./frontend/build/ user@ec2-instance:/var/www/html
+# Alternatively, you can use scp to copy the build files
+# scp -i $EC2_KEY_PATH -r ./frontend/build/ $EC2_USER@$EC2_IP:$EC2_DEST_DIR
